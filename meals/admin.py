77aa62e5cc09899beb98f5a1302ba0f20a87af50
GrigoriySZ <zaitsevgs@gmail.com>
2026-09-21 19:47:41 +0700

    feat: implement get_urls and import_excel methods in ProductAdmin

--- meals/admin.py
+++ meals/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin, messages
+from django.urls import path
 import openpyxl
 from django.shortcuts import render, redirect
 from .models import Product
@@ -10,3 +11,64 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'calories', 'proteins', 'fats', 'carbs',)
     search_fields = ('name',)
     change_list_template = 'meals/admin/product_change_list.html'
+
+    def get_urls(self):
+        """Кастомный url для импорта Excel"""
+
+        urls = super().get_urls()
+        custom_url = [
+            path('excel-import/', 
+                 self.admin_site.admin_view(self.import_excel), 
+                 name='product_import'), 
+        ]
+        return urls + custom_url
+    
+    def import_excel(self, request):
+        if request.method == 'POST':
+            form = ExcelInputForm(request.POST, request.FILES)
+            if form.is_valid():
+                excel_file = request.FIELS['excel_file']
+                try:
+                    workbook = openpyxl.load_workbook(excel_file)
+                    sheet = workbook.active
+                    created_count = 0
+                    # Куриное филе (сырое) 165 31,9 3,6 0,0
+
+                    for row in sheet.iter_rows(min_row=2, values_only=True):
+                        if not row or not row[0]:
+                            continue
+                        name = str(row[0]).strip
+                        calories = int(row[1])
+                        proteins = float(row[2])
+                        fats = float(row[3])
+                        carbs = float(row[4])
+
+                        Product.objects.create(
+                            name=name,
+                            calories=calories, 
+                            proteins=proteins, 
+                            fats=fats, 
+                            carbs=carbs
+                        )
+                        created_count += 1
+                    self.message_user(
+                        request,
+                        f'Вставлено строк: {created_count}', 
+                        messages.SUCCESS
+                    )
+                    return redirect('admin:product_change_list')
+                except Exception: 
+                    self.message_user(
+                        request,
+                        'Ошибка при обработке формы',
+                        messages.ERROR
+                    )
+        else:
+            form = ExcelInputForm()
+
+        context = {
+            **self.admin_site.each_context(request),
+            'form': form,
+            'title': 'Импоркт продуктов из Excel',
+        }
+        return render(request, 'meals/admin/excel_import.html', context)
