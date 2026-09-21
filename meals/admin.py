e1d6ca6d32aa154ea603eec4c19148f85e6e161d
GrigoriySZ <zaitsevgs@gmail.com>
2026-09-21 19:09:43 +0700

    feat: register Product in admin

--- meals/admin.py
+++ meals/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+import openpyxl
+from django.shortcuts import render, redirect
+from .models import Product
+from .forms import ExcelInputForm
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'calories', 'proteins', 'fats', 'carbs',)
+    search_fields = ('name',)
+    change_list_template = 'meals/admin/product_change_list.html'
